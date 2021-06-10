c529dceda4e7a8c6001cdb12b0bbb012be4a6fab
Cierra McDonald <cierralauren15@gmail.com>
2021-06-10 14:16:37 -0700

    Feature/use films hook (#2)
    * creating useFilms hook with dummy data
    
    * dummy data rendering to page
    
    * any changes before driver switch

--- src/components/films/Film.jsx
+++ src/components/films/Film.jsx
@@ -1,12 +1,19 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const Film = () => { 
+const Film = ({ name, image }) => { 
   return (
     <div>
-
+      <p>{name}</p>
+      <img src={image} alt=""/>
     </div>
   );
+};
+
+Film.propTypes = { 
+  name: PropTypes.string.isRequired,
+  image: PropTypes.string.isRequired
+
 };
 
 export default Film;
---
+++ src/components/hooks/useFilms.jsx
@@ -0,0 +1,19 @@
+import { useEffect, useState } from 'react';
+import { getCharacters } from '../../services/apiUtils';
+
+const useFilms = () => { 
+  const [films, setFilms] = useState([]);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => { 
+    getCharacters()
+      .then(films => setFilms(films))
+      .finally(() => setLoading(false));
+
+  }, []);
+
+  return { films, loading };
+
+};
+
+export default useFilms;
