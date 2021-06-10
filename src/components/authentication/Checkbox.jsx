bcc90833c54628501039cdcc79eac9f89a4d442b
Katilyn Wiggins <32716761+katilyn-wiggins@users.noreply.github.com>
2021-06-10 16:20:32 -0400

    updated oAuth (#1)

---
+++ src/components/authentication/Checkbox.jsx
@@ -0,0 +1,113 @@
+/* eslint-disable max-len */
+import React from 'react';
+import { withStyles } from '@material-ui/core/styles';
+import { blue } from '@material-ui/core/colors';
+import FormGroup from '@material-ui/core/FormGroup';
+import FormControlLabel from '@material-ui/core/FormControlLabel';
+import Checkbox from '@material-ui/core/Checkbox';
+// import Favorite from '@material-ui/icons/Favorite';
+// import FavoriteBorder from '@material-ui/icons/FavoriteBorder';
+import VideocamOutlinedIcon from '@material-ui/icons/VideocamOutlined';
+import VideocamIcon from '@material-ui/icons/Videocam';
+
+const VideoCheckbox = withStyles({
+  //change colors of checkbox
+  root: {
+    color: blue[800],
+    '&$checked': {
+      color: blue[800],
+    },
+  },
+  checked: {},
+})((props) => <Checkbox color="default" {...props} />);
+
+
+export default function CheckboxLabels() {
+  const [state, setState] = React.useState({
+    checkedA: true,
+    checkedB: true,
+    checkedF: true,
+    checkedH: true,
+  });
+
+  const handleChange = (event) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
+  return (
+    <FormGroup row>
+      <FormControlLabel
+        control={<VideoCheckbox icon={<VideocamOutlinedIcon />} 
+          checkedIcon={<VideocamIcon />} 
+          name="checkedA" checked={state.checkedA} 
+          onChange={handleChange}
+        />}
+        label="Documentary"
+      />
+      <FormControlLabel
+        control={<VideoCheckbox icon={<VideocamOutlinedIcon />} 
+          checkedIcon={<VideocamIcon />} 
+          name="checkedB" checked={state.checkedB} 
+          onChange={handleChange}
+        />}
+        label="Romance"
+      />
+      <FormControlLabel
+        control={<VideoCheckbox icon={<VideocamOutlinedIcon />} 
+          checkedIcon={<VideocamIcon />} 
+          name="checkedC" checked={state.checkedC} 
+          onChange={handleChange}
+        />}
+        label="Comedy"
+      />
+      <FormControlLabel
+        control={<VideoCheckbox icon={<VideocamOutlinedIcon />} 
+          checkedIcon={<VideocamIcon />} 
+          name="checkedD" checked={state.checkedD} 
+          onChange={handleChange}
+        />}
+        label="Horror"
+      />
+      <FormControlLabel
+        control={<VideoCheckbox icon={<VideocamOutlinedIcon />} 
+          checkedIcon={<VideocamIcon />} 
+          name="checkedD" checked={state.checkedD} 
+          onChange={handleChange}
+        />}
+        label="Thriller"
+      />
+      <FormControlLabel
+        control={<VideoCheckbox icon={<VideocamOutlinedIcon />} 
+          checkedIcon={<VideocamIcon />} 
+          name="checkedD" checked={state.checkedD} 
+          onChange={handleChange}
+        />}
+        label="Drama"
+      />
+      <FormControlLabel
+        control={<VideoCheckbox icon={<VideocamOutlinedIcon />} 
+          checkedIcon={<VideocamIcon />} 
+          name="checkedD" checked={state.checkedD} 
+          onChange={handleChange}
+        />}
+        label="Fantasy"
+      />
+      <FormControlLabel
+        control={<VideoCheckbox icon={<VideocamOutlinedIcon />} 
+          checkedIcon={<VideocamIcon />} 
+          name="checkedD" checked={state.checkedD} 
+          onChange={handleChange}
+        />}
+        label="Mystery"
+      />
+      <FormControlLabel
+        control={<VideoCheckbox icon={<VideocamOutlinedIcon />} 
+          checkedIcon={<VideocamIcon />} 
+          name="checkedD" checked={state.checkedD} 
+          onChange={handleChange}
+        />}
+        label="Western"
+      />
+    </FormGroup>
+  );
+}
--- src/components/authentication/Login.jsx
+++ src/components/authentication/Login.jsx
@@ -1,13 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { GoogleLogin, GoogleLogout } from 'react-google-login';
+// import { useLocation } from 'react-router';
+
+
 
 
 const Login = () => { 
+  const [uri, setUri] = useState(''); 
+  // const url = useLocation(); 
+
+
+  // const redirect = () => {
+
+  //   if(url === '/investor-registration') {
+  //     setUri('../../films/FilmListPage');  
+  //     return uri;
+  //   }
+  //   setUri('./FilmerApplication');  
+  //   return uri;
+  // }; 
+
+
 
   const responseGoogle = (response) => { 
     console.log(response);
     console.log(response.profileObj);
   };
+
 
   return ( 
     <div>
@@ -16,7 +35,13 @@ const Login = () => {
         buttonText="Login"
         onSuccess={responseGoogle}
         onFailure={responseGoogle}
-        cookiePolicy={'single_host_origin'}/>
+        cookiePolicy={'single_host_origin'}
+        uxMode="redirect"
+        redirectUri = 
+        "http://localhost:3000/film-application"
+        // {(url === '/filmer-registration')? "http://localhost:3000/filmer-application" : "http://localhost:3000/FilmsListPage"}
+      />
+
       <GoogleLogout
         clientId={process.env.GOOGLE_AUTH_CLIENT_ID}
         buttonText="Logout"
