0c6ecc65d8355371ebb8f143e283f6fbe54c2cd3
Zina Mohamed <zina.a.mohamed@gmail.com>
2021-06-10 14:47:24 -0700

    implemented upload button and state changes

--- src/components/authentication/FilmerApplication.jsx
+++ src/components/authentication/FilmerApplication.jsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import TextField from '@material-ui/core/TextField';
 import Checkbox from './Checkbox'; 
 import SubmitButton from './SubmitButton';
 import TextArea from './TextArea';
+import UploadButton from './UploadButton';
+import './form.css';
+
 
 
 const useStyles = makeStyles((theme) => ({
@@ -17,15 +20,22 @@ const useStyles = makeStyles((theme) => ({
 
 const FilmerApplication = () => {
   const classes = useStyles();
+  const [value, setValue] = useState('');
 
+  const handleChange = (event) => {
+    console.log(event.target.value);
+    setValue(event.target.value);
+  };
+
+  
   return (
     <form className={classes.root}>
-      <TextField id="standard-basic" label="Title" />
-      <TextField id="standard-basic" label="Directors Name" />
-      <TextField id="standard-basic" label="$ Budget" />
-      <TextField id="standard-basic" label="Trailer URL" />
+      <TextField id="standard-basic" label="Title" onChange={handleChange}/>
+      <TextField id="standard-basic" label="Directors Name" onChange={handleChange}/>
+      <TextField id="standard-basic" label="$ Budget" onChange={handleChange}/>
+      <TextField id="standard-basic" label="Trailer URL" onChange={handleChange}/>
       <TextArea />
-      {/* image uploader*/}
+      <UploadButton/>
       <Checkbox /> 
       <SubmitButton />
     </form>
