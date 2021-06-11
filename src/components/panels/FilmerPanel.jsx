0185581e646c527a6dd563bc43a99e919464d166
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-06-11 19:44:21 -0400

    investor panel update

--- src/components/panels/FilmerPanel.jsx
+++ src/components/panels/FilmerPanel.jsx
@@ -3,12 +3,10 @@ import React from 'react';
 import PanelTextField from './PanelTextField'; 
 import DeleteProfileButton from './DeleteProfileButton'; 
 import FilmerSubmissionTable from './FilmerSubmissionTable'; 
-import FilmData from '../../components/films/mockDataFilm'; 
 
 import filmer from './mockfilmer'; 
 
 const FilmerPanel = () => {
-
 
   return (
     <>
@@ -16,7 +14,7 @@ const FilmerPanel = () => {
       <h1>User Information</h1>
       <PanelTextField {...filmer} /> 
       <h1>Short Film Submissions</h1>
-      <FilmerSubmissionTable {...[FilmData]}/> 
+      <FilmerSubmissionTable/> 
       <DeleteProfileButton /> 
       <p>Donations will not be refunded, please contact support@angle-on.com if you have further questions</p>
       
--- src/components/panels/InvestorPanel.jsx
+++ src/components/panels/InvestorPanel.jsx
@@ -1,61 +1,26 @@
 /* eslint-disable max-len */
-import React, { useState } from 'react';
-import PanelTextField from './PanelTextField'; 
-// import PanelButtons from './PanelButtons'; 
+import React from 'react';
+import InvestorPanelTextField from './InvestorPanelTextField'; 
 import DeleteProfileButton from './DeleteProfileButton'; 
-import Styles from './investor.css'; 
 import DonationTable from './DonationTable'; 
-import { makeStyles } from '@material-ui/core/styles';
-import Button from '@material-ui/core/Button';
-import { updateInvestorById } from '../../services/apiUtils'; 
 
+import investor from './mockinvestor';
 // import useInvestor from '../hooks/useInvestor'; 
-
-import investor from './mockinvestor'; 
 // import donationData from './mockdonationinfo'; 
 
-
-const useStyles = makeStyles((theme) => ({
-  root: {
-    '& > *': {
-      margin: theme.spacing(1),
-    },
-  },
-}));
-
-
 const InvestorPanel = () => {
-  const [edit, setEdit] = useState(true); 
-  const classes = useStyles();
-
-  const handleClick = () => {
-    setEdit(false); 
-    console.log(edit, 'in handle click'); 
-    return edit; 
-  }; 
-
-  const handleUpdate = () => {
-    updateInvestorById(); 
-    console.log('in handle update'); 
-  }; 
-
 
   return (
-    <form className={Styles.investor}>
+    <>
       <h1>Investor Panel</h1>
       <h1>User Information</h1>
-      <PanelTextField {...investor} edit={edit} /> 
-      {/* <PanelButtons />  */}
-      <div className={classes.root}>
-        <Button variant="contained" onClick={handleClick}>Edit</Button>
-        <Button variant="contained" onClick={handleUpdate}>Update</Button>
-      </div>
+      <InvestorPanelTextField {...investor} /> 
       <h1>Donations</h1>
       <DonationTable/> 
       <DeleteProfileButton /> 
       <p>Donations will not be refunded, please contact support@angle-on.com if you have further questions</p>
       
-    </form>
+    </>
   );
 }; 
 export default InvestorPanel;
