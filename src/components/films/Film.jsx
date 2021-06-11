036d0ec0e4d6a85085879597afcb351663d1b8f8
Cierra McDonald <cierralauren15@gmail.com>
2021-06-10 19:34:42 -0700

    Form feauture (#3)
    * implemented upload button and state changes
    
    * munging o-auth data
    
    * aws image upload
    
    Co-authored-by: Zina Mohamed <zina.a.mohamed@gmail.com>

--- src/components/films/Film.jsx
+++ src/components/films/Film.jsx
@@ -1,12 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './films.css';
 
 const Film = ({ name, image }) => { 
   return (
-    <div>
-      <p>{name}</p>
-      <img src={image} alt=""/>
-    </div>
+    <ul className={styles.ul}>
+      <li><p>{name}</p></li>
+      <li><img src={image} alt=""/></li>
+    </ul>
   );
 };
 
