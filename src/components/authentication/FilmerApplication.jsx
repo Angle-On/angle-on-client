3b7cabfb3138925ccc9164e1789a52708f60cff4
cmazariegos44 <41842292+cmazariegos44@users.noreply.github.com>
2021-06-15 11:37:38 -0700

    Claudia3 (#22)
    * added heroku url
    
    * added heroku url
    
    * one last 7890
    
    * one last 7890
    
    * deleted film slash

--- src/components/authentication/FilmerApplication.jsx
+++ src/components/authentication/FilmerApplication.jsx
@@ -73,6 +73,7 @@ const FilmerApplication = () => {
   //           'Content-Type': `multipart/form-data; boundary=${data._boundary}`,
   //         },
   //       })
+  
   //       .then((response) => {
   //         if (200 === response.status) {
   //           // If file size is larger than expected.
@@ -135,7 +136,7 @@ const FilmerApplication = () => {
       const DEVURL = 'https://secure-taiga-78931.herokuapp.com';
       const URL = DEVURL;
       axios
-        .post(`${URL}/api/v1/films/`, data, {
+        .post(`${URL}/api/v1/films`, data, {
           headers: {
             accept: 'application/json',
             'Accept-Language': 'en-US,en;q=0.8',
