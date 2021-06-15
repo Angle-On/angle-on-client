95530d91d12f3591f4ffcfd2835fd33599cef9a5
Claudia <claudias.mazariegos@gmail.com>
2021-06-15 11:04:12 -0700

    added heroku url

--- src/components/authentication/FilmerApplication.jsx
+++ src/components/authentication/FilmerApplication.jsx
@@ -90,7 +90,9 @@ const FilmerApplication = () => {
       const DEVURL = 'https://secure-taiga-78931.herokuapp.com';
       const URL = DEVURL;
       axios
-        .post(`${URL}/api/v1/films`, data, {
+      const DEVURL = 'https://secure-taiga-78931.herokuapp.com';
+      const URL = DEVURL;
+          .post(`${URL}/api/v1/films/`, data, {
           headers: {
             accept: 'application/json',
             'Accept-Language': 'en-US,en;q=0.8',
