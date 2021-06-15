b34dfd20a355ce54a0297cfcd2bc7c99ebd8dffb
cmazariegos44 <41842292+cmazariegos44@users.noreply.github.com>
2021-06-15 12:45:40 -0700

    Claudia3 (#25)
    * added heroku url
    
    * added heroku url
    
    * one last 7890
    
    * one last 7890
    
    * deleted film slash
    
    * took out window relocation
    
    * .
    
    * cors issue
    
    * cors issue
    
    * added heroku url
    
    * .

--- src/components/authentication/FilmerApplication.jsx
+++ src/components/authentication/FilmerApplication.jsx
@@ -90,7 +90,7 @@ const FilmerApplication = () => {
       const DEVURL = 'https://secure-taiga-78931.herokuapp.com';
       const URL = DEVURL;
       axios
-        .post(`${URL}/api/v1/films`, data, {
+        .post(`${URL}/api/v1/films/`, data, {
           headers: {
             accept: 'application/json',
             'Accept-Language': 'en-US,en;q=0.8',
