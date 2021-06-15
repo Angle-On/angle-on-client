ea10d851474c17ff2b66f14255c43e4781d57c35
Claudia <claudias.mazariegos@gmail.com>
2021-06-15 13:42:50 -0700

    cors issue

--- src/services/apiUtils.js
+++ src/services/apiUtils.js
@@ -1,7 +1,6 @@
 import FilmData from '../components/films/mockDataFilm';
 
-// eslint-disable-next-line max-len
-const DEVURL = 'https://cors-anywhere.herokuapp.com/https://secure-taiga-78931.herokuapp.com';
+const DEVURL = 'https://secure-taiga-78931.herokuapp.com';
 const URL = DEVURL;
 
 export const getCharacters = async () => {
