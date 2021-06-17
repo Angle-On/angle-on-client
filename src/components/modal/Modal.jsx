fd6fb73ff2d3b79d6084f100def4c072baade0f4
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-06-17 19:27:22 -0400

    added first and last name to modal

--- src/components/modal/Modal.jsx
+++ src/components/modal/Modal.jsx
@@ -12,11 +12,10 @@ import {
   CloseModalButton, 
   OpaqueCover,
 } from './ModalStyling'; 
-import {AlternateEmailTwoTone} from '@material-ui/icons';
 
 
 
-const Modal = ({ showModal, setShowModal, films_description, films_image, films_name, films_budget, films_url, director_name, films_id }) => { 
+const Modal = ({ showModal, setShowModal, films_description, films_image, films_name, films_budget, films_url, director_first_name, director_last_name, films_id }) => { 
   const history = useHistory();
 
  
@@ -69,7 +68,7 @@ const Modal = ({ showModal, setShowModal, films_description, films_image, films_
               <ModalContent>
                 <h5>{films_id}</h5>
                 <h1>{films_name}</h1>
-                <h4>Created By: {director_name}</h4>
+                <h4>Created By: {director_first_name} {director_last_name}</h4>
                 <p>Summary: {films_description}</p>
                 <p>Short Film Budget: ${films_budget}.00</p>
                 <a href={films_url}>Short Film Link</a>
