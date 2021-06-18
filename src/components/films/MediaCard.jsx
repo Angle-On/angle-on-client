42d9b5278dd87998820cb04407f4cf473e723a76
Cierra-McDonald <cierralauren15@gmail.com>
2021-06-17 20:03:07 -0700

    media cards for list page

--- src/components/films/MediaCard.jsx
+++ src/components/films/MediaCard.jsx
@@ -9,28 +9,28 @@ import CardMedia from '@material-ui/core/CardMedia';
 import Typography from '@material-ui/core/Typography';
 import ModalParent from '../modal/ModalParent';
 import SocialButtons from '../socialbuttons/SocialButtons';
+import PropTypes from 'prop-types';
 
 const useStyles = makeStyles({
   root: {
     maxWidth: 345,
-    display: "flex",
-    flexDirection: "row",
-    flexWrap: "wrap",
-    margin: "50px",
-    zIndex:"-1",
-    height: "500px",
-    width: "500px",   
-    textAlign: "center",
-    justifyContent: "center",
-    alignItems: "center",
-    margin: 50,
-    backgroundColor: "#f9f9f9", 
-    boxShadow: "rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px",
+    display: 'flex',
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+    margin: '50px',
+    zIndex:'-1',
+    height: '500px',
+    width: '500px',   
+    textAlign: 'center',
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: '#f9f9f9', 
+    boxShadow: 'rgba(14, 30, 37, 0.12) 0px 2px 4px 0px, rgba(14, 30, 37, 0.32) 0px 2px 16px 0px',
   },
   media: {
     height: 140,
-    display: "flex",
-    flexDirection: "column",
+    display: 'flex',
+    flexDirection: 'column',
     margin: 20, 
   },
   image: {
@@ -41,7 +41,19 @@ const useStyles = makeStyles({
   }
 });
 
-export default function MediaCard({ films_name, films_image, films_description, films_budget, films_url, films_genre, director_name, films_id }) {
+export default function MediaCard({ films_name,
+  films_image,
+  films_description,
+  films_budget,
+  films_url,
+  films_genre,
+  director_name,
+  films_id,
+  director_first_name,
+  director_last_name,
+  director_id
+
+}) {
   const classes = useStyles();
   
   return (
@@ -61,7 +73,7 @@ export default function MediaCard({ films_name, films_image, films_description, 
           </Typography>
           <Typography gutterBottom variant="body" component="h5"
             className={classes.text}>
-            Created By:{director_name}
+            Created By: {director_first_name} {director_last_name}
           </Typography>
         </CardContent>
       </CardActionArea>
@@ -75,9 +87,26 @@ export default function MediaCard({ films_name, films_image, films_description, 
           films_genre={films_genre}
           director_name={director_name}
           films_id={films_id}
+          director_first_name={director_first_name}
+          director_last_name={director_last_name}
         />
         <SocialButtons></SocialButtons>
       </CardActions>
     </Card>
   );
 }
+
+MediaCard.propTypes = { 
+  films_name: PropTypes.string.isRequired,
+  films_image: PropTypes.string.isRequired,
+  films_description: PropTypes.string.isRequired,
+  films_budget: PropTypes.number.isRequired,
+  films_url: PropTypes.string.isRequired,
+  films_genre: PropTypes.array.isRequired,
+  director_name: PropTypes.string.isRequired,
+  director_first_name: PropTypes.string.isRequired,
+  director_last_name: PropTypes.string.isRequired,
+  director_id: PropTypes.number.isRequired, 
+  films_id: PropTypes.number.isRequired
+
+};
