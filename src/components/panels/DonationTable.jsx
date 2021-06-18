bf8532e40b3fabb9fbf0d291f5eeca58cf8de651
KATILYN WIGGINS <KATILYNSWIGGINS@GMAIL.COM>
2021-06-17 21:16:32 -0400

    brackets man...

--- src/components/panels/DonationTable.jsx
+++ src/components/panels/DonationTable.jsx
@@ -7,7 +7,7 @@ import TableContainer from '@material-ui/core/TableContainer';
 import TableHead from '@material-ui/core/TableHead';
 import TableRow from '@material-ui/core/TableRow';
 import Paper from '@material-ui/core/Paper';
-import getDonationByInvestorId from '../../services/apiUtils'; 
+import { getDonationByInvestorId } from '../../services/apiUtils'; 
 
 
 //when the time comes, use hook 
