2f434c98d1cc131f3a15c93cc7e3b7454a02b40b
Zina Mohamed <zina.a.mohamed@gmail.com>
2021-06-18 08:40:42 -0700

    populated landing page content and added footer

--- src/components/home/Data.jsx
+++ src/components/home/Data.jsx
@@ -1,18 +1,18 @@
 export const homeObjOne = {
   id: 'about',
-  lightBg: false,
-  lightText: true,
-  lightTextDesc: true,
-  topLine: 'Lorem ipsum',
-  headline: 'Lorem ipsum dolor sit amet',
+  lightBg: true,
+  lightText: false,
+  lightTextDesc: false,
+  topLine: 'HOW IT WORKS',
+  headline: 'Get Started',
   description:
     'These filmmakers need the support of their community to produce their short films. Check out our films page to learn more about the shorts and to donate to a project.',
   buttonLabel: 'Get Started',
   imgStart: false,
   alt: 'Car',
-  dark: true,
-  primary: true,
-  darkText: false
+  dark: false,
+  primary: false,
+  darkText: true
 };
 
 export const homeObjTwo = {
@@ -20,10 +20,10 @@ export const homeObjTwo = {
   lightBg: true,
   lightText: false,
   lightTextDesc: false,
-  topLine: 'Lorem ipsum',
-  headline: 'Lorem ipsum dolor sit amet',
+  topLine: 'ABOUT US',
+  headline: 'Our Mission',
   description:
-    'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore.',
+    'Our mission is to move the lens onto individuals from underrepresented communities and focus on their creativity and talent. Our organization supports equality, equity, and economic empowerment for all',
   buttonLabel: 'Learn More',
   imgStart: true,
   alt: 'Piggybank',
---
+++ src/components/home/Footer.js
@@ -0,0 +1,93 @@
+import React from 'react';
+import {
+  FaFacebook,
+  FaInstagram,
+  FaYoutube,
+  FaTwitter,
+  FaLinkedin
+} from 'react-icons/fa';
+import {
+  FooterContainer,
+  FooterWrap,
+  FooterLinksContainer,
+  FooterLinksWrapper,
+  FooterLinkItems,
+  FooterLinkTitle,
+  FooterLink,
+  SocialMedia,
+  SocialMediaWrap,
+  SocialLogo,
+  WebsiteRights,
+  SocialIcons,
+  SocialIconLink
+} from './FooterElements';
+
+const Footer = () => {
+  return (
+    <FooterContainer>
+      <FooterWrap>
+        <FooterLinksContainer>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>About Us</FooterLinkTitle>
+              <FooterLink to='/'>Testimonials</FooterLink>
+              <FooterLink to='/'>Careers</FooterLink>
+              <FooterLink to='/'>Investors</FooterLink>
+              <FooterLink to='/'>Terms of Service</FooterLink>
+            </FooterLinkItems>
+            <FooterLinkItems>
+              <FooterLinkTitle>Contact Us</FooterLinkTitle>
+              <FooterLink to='/'>Contact</FooterLink>
+              <FooterLink to='/'>Support</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+          <FooterLinksWrapper>
+            <FooterLinkItems>
+              <FooterLinkTitle>Social Media</FooterLinkTitle>
+              <FooterLink to='/'>Instagram</FooterLink>
+              <FooterLink to='/'>Facebook</FooterLink>
+              <FooterLink to='/'>Youtube</FooterLink>
+              <FooterLink to='/'>Twitter</FooterLink>
+            </FooterLinkItems>
+          </FooterLinksWrapper>
+        </FooterLinksContainer>
+        <SocialMedia>
+          <SocialMediaWrap>
+            <SocialLogo>
+              AngleOn
+            </SocialLogo>
+            <WebsiteRights>AngleOn © 2021 All rights reserved.</WebsiteRights>
+            <SocialIcons>
+              <SocialIconLink href='/' target='_blank' aria-label='Facebook'>
+                <FaFacebook />
+              </SocialIconLink>
+              <SocialIconLink href='/' target='_blank' aria-label='Instagram'>
+                <FaInstagram />
+              </SocialIconLink>
+              <SocialIconLink
+                href='/'
+                target='_blank'
+                aria-label='Youtube'
+                rel='noopener noreferrer'
+              >
+                <FaYoutube />
+              </SocialIconLink>
+              <SocialIconLink
+                target='_blank'
+                aria-label='Twitter'
+                href='/'
+              >
+                <FaTwitter />
+              </SocialIconLink>
+              <SocialIconLink href='/' target='_blank' aria-label='Linkedin'>
+                <FaLinkedin />
+              </SocialIconLink>
+            </SocialIcons>
+          </SocialMediaWrap>
+        </SocialMedia>
+      </FooterWrap>
+    </FooterContainer>
+  );
+  };
+
+export default Footer;
